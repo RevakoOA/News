8ca1f8acacef76c0de69202da48fd3fb72c189f2
revakoOA <revakoOA@gmail.com>
2018-09-06 20:10:34 +0300

    Images added. Toolbar in progress.

--- app/src/main/java/com/just_me/news/news/MainActivity.kt
+++ app/src/main/java/com/just_me/news/news/MainActivity.kt
@@ -2,11 +2,28 @@ package com.just_me.news.news
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
+import android.R.menu
+import android.view.Menu
+import android.view.MenuInflater
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar as Toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.example_menu, menu)
+        return true
     }
 }
