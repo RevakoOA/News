9073a796cfcdb02aec9ea8ef1c6c6c8059ad00bd
revakoOA <revakoOA@gmail.com>
2018-09-10 13:59:01 +0300

    Item click in recycler added.

--- app/src/main/java/com/just_me/news/MyRecyclerAdapter.kt
+++ app/src/main/java/com/just_me/news/MyRecyclerAdapter.kt
@@ -2,6 +2,8 @@ package com.just_me.news
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import com.just_me.news.core.extension.inflate
 import com.just_me.news.core.extension.loadFromUrl
@@ -9,10 +11,13 @@ import com.just_me.news.news.R
 import kotlinx.android.synthetic.main.item_recycler.view.*
 
 class MyRecyclerAdapter(val items: ArrayList<RecyclerData>): RecyclerView.Adapter<MyRecyclerAdapter.ViewHolder>() {
-    lateinit var itemsToShow: ArrayList<RecyclerData>
+    var itemsToShow: ArrayList<RecyclerData>
+    var itemsIsOpen: ArrayList<Boolean>
     init {
         itemsToShow = ArrayList(items.size)
         itemsToShow.addAll(items)
+        itemsIsOpen = ArrayList(items.size)
+        itemsIsOpen.addAll(items.map { false })
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -23,23 +28,31 @@ class MyRecyclerAdapter(val items: ArrayList<RecyclerData>): RecyclerView.Adapte
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val data = itemsToShow[position]
-        holder.itemView.tvName.text = data.title
+        val isOpen = itemsIsOpen[position]
         holder.itemView.imageView.loadFromUrl(data.image)
+        holder.itemView.tvName.text = data.title
+        holder.itemView.tvText.text = data.about
+        holder.itemView.tvText.visibility = if (isOpen) VISIBLE else GONE
+        holder.itemView.llContent.setOnClickListener {
+            it.tvText.visibility = if (it.tvText.visibility == GONE) VISIBLE else GONE
+            itemsIsOpen[position] = it.tvText.visibility == VISIBLE
+            notifyItemChanged(position)
+        }
     }
 
     fun filterDatas(s: String) {
         itemsToShow.clear()
+        itemsIsOpen.clear()
         for (item in items) {
             if (item.title.contains(s, true)) {
                 itemsToShow.add(item)
+                itemsIsOpen.add(false)
             }
         }
         notifyDataSetChanged()
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind() {
+    class ViewHolder(iv: View) : RecyclerView.ViewHolder(iv) {
 
-        }
     }
 }
